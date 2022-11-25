f2796a5e089ca480515f1543c824fbb96d5db11a
emberati <alex.shak55@gmail.com>
2022-11-26 01:16:22 +0300

    Реализован класс менеджер твиков

--- src/main/java/com/pixplaze/plugin/tweak/TweakManager.java
+++ src/main/java/com/pixplaze/plugin/tweak/TweakManager.java
@@ -1,39 +1,57 @@
 package com.pixplaze.plugin.tweak;
 
 import com.pixplaze.plugin.PixplazeServerTweaks;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.HandlerList;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
-public class TweakManager {
+public final class TweakManager {
+
 
     private final PixplazeServerTweaks plugin;
-    private static final Set<ServerTweak> availableTweaks;
-    private static final Map<String, ServerTweak> loadedTweaks;
+    private final Set<ServerTweak> availableTweaks;
+    private final Map<String, ServerTweak> loadedTweaks;
 
-    static {
-        availableTweaks = new HashSet<>(Set.of(
+    private TweakManager(PixplazeServerTweaks plugin) {
+        this(plugin, new HashSet<>(Set.of(
                 new PermeableItemFrames(),
                 new HelloMessage()
-        ));
-        loadedTweaks = new HashMap<>();
+        )));
     }
 
-    public TweakManager() {
-        plugin = PixplazeServerTweaks.getInstance().orElseThrow();
-        availableTweaks.forEach(tweak -> loadedTweaks.put(tweak.getTweakName(), tweak));
+    private TweakManager(PixplazeServerTweaks plugin, Set<ServerTweak> tweaks) {
+        this.plugin = plugin;
+        this.availableTweaks = tweaks;
+        this.loadedTweaks = new HashMap<>();
+    }
+
+    public void loadTweaks(FileConfiguration config) {
+        availableTweaks.forEach(tweak -> {
+            var tweakName = tweak.getTweakName();
+            var isTweakEnabled = config.getBoolean(tweakName);
+            if (isTweakEnabled) enableTweak(tweak);
+            loadedTweaks.put(tweak.getTweakName(), tweak);
+        });
     }
 
     public void enableTweak(ServerTweak tweak) {
+        tweak.setEnabled(true);
         plugin.registerListener(tweak);
     }
 
+    public void enableTweak(String tweakName) {
+        enableTweak(getTweak(tweakName));
+    }
+
     public void disableTweak(ServerTweak tweak) {
+        tweak.setEnabled(false);
         HandlerList.unregisterAll(tweak);
+    }
+
+    public void disableTweak(String tweakName) {
+        disableTweak(getTweak(tweakName));
     }
 
     public Set<ServerTweak> getLoadedTweaks() {
@@ -44,5 +62,9 @@ public class TweakManager {
         return availableTweaks.stream()
                 .filter(ServerTweak::isEnabled)
                 .collect(Collectors.toSet());
+    }
+
+    public ServerTweak getTweak(String tweakName) {
+        return loadedTweaks.get(tweakName);
     }
 }
