19303655d39e106f8105e242db362bcb8373a929
emberati <alex.shak55@gmail.com>
2022-12-26 02:05:42 +0300

    Реализован класс `StringUtils` для работы со строками при рефлексии

---
+++ src/main/java/com/pixplaze/plugin/reflected/StringUtils.java
@@ -0,0 +1,41 @@
+package com.pixplaze.plugin.reflected;
+
+import java.util.Arrays;
+
+public class StringUtils {
+    public static String toStringMethodArguments(String methodName, Class<?>[] argumentTypes) {
+        return toStringMethodArguments(methodName, argumentTypes, false);
+    }
+
+    public static String toStringMethodArguments(String methodName, Class<?>[] argumentTypes, boolean verbose) {
+        return Arrays.stream(argumentTypes)
+                .map(classType -> {
+                    if (verbose) {
+                        return classType.getTypeName();
+                    } else {
+                        var classTypeName = classType.getTypeName();
+                        var splitClassTypeName = classTypeName.split("\\.");
+                        return splitClassTypeName[splitClassTypeName.length - 1];
+                    }
+                })
+                .reduce((curr, next) -> curr + ", " + next)
+                .map(args -> "%s(%s)".formatted(methodName, args))
+                .orElse("%s()".formatted(methodName));
+    }
+
+    public static String toStringTypes(Object ... objects) {
+        return Arrays.stream(objects)
+                .map(Object::getClass)
+                .map(Class::getSimpleName)
+                .reduce((curr, next) -> String.join(", "))
+                .orElse("");
+    }
+
+    public static String toStringTypesFullNames(Object ... objects) {
+        return Arrays.stream(objects)
+                .map(Object::getClass)
+                .map(Class::getTypeName)
+                .reduce((curr, next) -> String.join(", "))
+                .orElse("");
+    }
+}
